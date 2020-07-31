ae32d3be09f7e724b920ded13f0ce3b96ba71051
Winston R. Milling <Winston@Milli.ng>
2020-07-31 15:46:30 -0400

    Add a few more utilities and mesa/glx

--- packages.nix
+++ packages.nix
@@ -4,10 +4,20 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    # Basic Terminal Stuff
     vim
     htop
     tmux
     git
+    yadm
+    nvme-cli
+    zsh
+
+    # Useful Applications
     firefox
+
+    # Graphics Stuff
+    mesa
+    glxinfo
   ];
 }
