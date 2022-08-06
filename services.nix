9ad4817604e393fbe2c551e49e9521f07c1d05b6
Winston R. Milling <Winston@Milli.ng>
2022-08-05 23:36:12 -0500

    NVMe move mount

--- services.nix
+++ services.nix
@@ -7,7 +7,7 @@
     unitConfig.ConditionPathExists = [ "" "/dev/nvme0" ];
     script = ''
       ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/nvme0n1 nvmecrypt --key-file /home/luks/nvme.key
-      ${pkgs.mount}/bin/mount /dev/mapper/nvmecrypt /NVMe
+      ${pkgs.mount}/bin/mount /dev/mapper/nvmecrypt /mnt/NVMe
     '';
   };
 }
