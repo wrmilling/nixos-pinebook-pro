ed299ce77c42f75b9e8ccd49778aa51353238864
Winston R. Milling <Winston@Milli.ng>
2022-08-05 23:51:56 -0500

    Try and make mount smarter

--- services.nix
+++ services.nix
@@ -6,8 +6,14 @@
     wantedBy = [ "multi-user.target" ];
     unitConfig.ConditionPathExists = [ "" "/dev/nvme0" ];
     script = ''
-      ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/nvme0n1 nvmecrypt --key-file /home/luks/nvme.key
-      ${pkgs.mount}/bin/mount /dev/mapper/nvmecrypt /mnt/NVMe
+      is_mounted() {
+        ${pkgs.mount}/bin/mount | ${pkgs.gawk}/bin/awk -v DIR="$1" '{if ($3 == DIR) { exit 0}} ENDFILE{exit -1}'
+      }
+
+      if ! is_mounted "/mnt/NVMe"; then
+        ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/nvme0n1 nvmecrypt --key-file /home/luks/nvme.key
+        ${pkgs.mount}/bin/mount /dev/mapper/nvmecrypt /mnt/NVMe
+      fi
     '';
   };
 }
