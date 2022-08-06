b8b7793f03c4927ca97f052c92751f89fbef6ce0
Winston R. Milling <Winston@Milli.ng>
2022-08-05 22:54:35 -0500

    Dont forget to do it the nix way

--- services.nix
+++ services.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  systemd.services.nvme-mount = { 
+  systemd.services.nvme-mount = {
     description = "Mount encrypted NVMe for PBP";
     wantedBy = [ "multi-user.target" ];
     unitConfig.ConditionPathExists = [ "" "/dev/nvme0" ];
     script = ''
-      cryptsetup luksOpen /dev/nvme0n1 nvmecrypt --key-file /home/luks/nvme.key
-      mount /dev/mapper/nvmecrypt /NVMe
+      ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/nvme0n1 nvmecrypt --key-file /home/luks/nvme.key
+      ${pkgs.mount}/bin/mount /dev/mapper/nvmecrypt /NVMe
     '';
   };
 }
